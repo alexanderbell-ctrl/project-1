fac0af3c04fdf85eeb6276624f8a9c7ae6a57379
alexanderbell-ctrl <alexander.bell@bsu.edu>
2026-01-09 23:58:55 +0000

    project 1 done!

---
+++ project1/hello.py
@@ -0,0 +1,4 @@
+user_name = input("What is your name?: ")
+user_color = input("What is your favorite color?: ")
+
+print(f"Hello {user_name}! Your favorite color is {user_color} I see...\n Mine is ocean spray green.")
---
+++ project1/readme.md
@@ -0,0 +1,6 @@
+#Name
+My name is **Alex**
+##Semester
+I would *like to learn* different ways you can apply programming skills.
+###Fact
+I'm a biochem major ~~with~~ a CS minor
